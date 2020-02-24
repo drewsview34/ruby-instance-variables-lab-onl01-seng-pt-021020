ba16065004047cf26357c201c94092e60547781f
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 21:03:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog
   #setter method sets a property for us and must look like this "method="
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   #getter method gets a property for us
   def name
-    this_dogs_name
+    @this_dogs_name
   end
 end
 
