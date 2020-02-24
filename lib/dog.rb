5496bfbfc2defb7db076e6d7401b520776f5f836
Andrew Hinojosa <drews_view@hotmail.com>
2020-02-24 20:56:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,16 @@
-class Dog 
-  def name=(dog_name) #setting method
+class Dog
+  #setter method sets a property for us and must look like this "method="
+  def name=(dog_name)
     this_dogs_name = dog_name
   end
   
-  #getting method
+  #getter method gets a property for us
   def name
     this_dogs_name
   end
 end
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+lassie.name
